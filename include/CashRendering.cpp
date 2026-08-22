a1f741008d223c38425a0beb433b5c7948a2564e
Charles <tonykart84@gmail.com>
2026-08-21 18:01:26 -0700

    BROKEN partial work with bgfx but I don't think it has constant buffers of gernic type

--- include/CashRendering.cpp
+++ include/CashRendering.cpp
@@ -11,12 +11,12 @@
 
 Renderer gfx;
 
-struct DxRenderer {
+struct GfxDevice {
     bgfx::VertexLayout vert_layouts[VertexType_Count] = {};
     bgfx::VertexBufferHandle fullscreen_verts;
     bgfx::VertexBufferHandle fullscreen_verts;
 };
-static DxRenderer s_gfx;
+static GfxDevice s_gfx;
 
 
 bool RenderInitBgfx()
@@ -65,7 +65,7 @@ bool RenderInitBgfx()
 
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL, ToColorI(backgroundColor).rgba, 1.0f, 0);
 
-    
+
     static_assert(VertexType_Count == 2); //NOTE(CSH): If this is wrong then you will need to update the vertex layout descriptions here:
     {
         //Vertex_2D
@@ -249,3 +249,309 @@ void DeleteTexture(Texture** texture)
 
 }
 void* GetShaderResourceView(TextureIndex t);
+
+
+
+
+
+//========================
+//       GpuBuffer
+//========================
+
+    //s_gfx.fullscreen_verts = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+
+struct GfxGpuBuffer : public GpuBuffer
+{
+    union Handle {
+        bgfx::DynamicIndexBufferHandle  dynamic_index_buffer;
+        bgfx::DynamicVertexBufferHandle dynamic_vertex_buffer;
+        //bgfx::FrameBufferHandle         frame_buffer;
+        bgfx::IndexBufferHandle         index_buffer;
+        bgfx::IndirectBufferHandle      indirect_buffer;
+        //bgfx::OcclusionQueryHandle      occlusion_query;
+        //bgfx::ProgramHandle             program;
+        //bgfx::ShaderHandle              shader;
+        //bgfx::TextureHandle             texture;
+        //bgfx::UniformHandle             uniform;
+        bgfx::VertexBufferHandle        vertex_buffer;
+        bgfx::VertexLayoutHandle        vertex_layout;
+    }handle;
+
+
+
+    //D3D11_USAGE m_usage = D3D11_USAGE_DYNAMIC;
+    ID3D11Buffer* buffer = nullptr;
+    ID3D11ShaderResourceView* structure_resource_view = nullptr;
+    ID3D11UnorderedAccessView* unordered_access_view = nullptr;
+    //D3D11_BIND_FLAG m_target = {};
+};
+
+void GpuBuffer::Upload(const void* data, const size_t in_count, const u32 in_element_size, const bool is_byte_format)
+{
+    GfxGpuBuffer* buf = reinterpret_cast<GfxGpuBuffer*>(this);
+    count = in_count;
+    element_size = in_element_size;
+    SafeRelease(buf->buffer);
+    VALIDATE(element_size);
+    VALIDATE(buf->type != GpuBufferType_Invalid);
+    VALIDATE(in_count);
+    UINT total_bytes = UINT(element_size * in_count);
+    //ASSERT(total_bytes / 16 == 0);
+    UINT buffer_type = 0;
+    UINT cpu_access_flags = 0;
+    UINT struct_byte_stride = 0;
+    UINT memory_pitch = 0;
+    UINT misc_flags = 0;
+    UINT uav_flags = 0;
+    D3D11_USAGE usage_flag = buf->is_dymamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
+    DXGI_FORMAT format = DXGI_FORMAT_UNKNOWN;
+    bool create_srv = false;
+    bool create_uav = false;
+
+
+
+
+    switch (buf->type)
+    {
+    case GpuBufferType_Vertex:
+        buf->handle.vertex_buffer           = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+        buf->handle.dynamic_vertex_buffer   = bgfx::createDynamicVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+    case GpuBufferType_Index:
+        buf->handle.index_buffer            = bgfx::createIndexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+        buf->handle.dynamic_index_buffer    = bgfx::createDynamicIndexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+    case GpuBufferType_Constant:
+    //NOTE(CSH): NO CONSTANT BUFFER OF GENERIC TYPE!!??
+        buf->handle.uniform = bgfx::createUniform(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+    case GpuBufferType_Structure:
+        buf->handle.vertex_buffer = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+        buf->handle.vertex_buffer = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+        buf->handle.vertex_buffer = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+        buf->handle.vertex_buffer = bgfx::createVertexBuffer(bgfx::makeRef(verts, sizeof(verts)), s_gfx.vert_layouts[VertexType_2D], BGFX_BUFFER_NONE);
+    case GpuBufferType_RWStructure:
+    case GpuBufferType_AppendStructure:
+    case GpuBufferType_IndirectArgs:
+    default: FAIL; return false;
+    }
+
+
+
+
+
+
+    switch (buf->type)
+    {
+    case GpuBufferType_Vertex:
+        buffer_type = D3D11_BIND_VERTEX_BUFFER;
+        break;
+    case GpuBufferType_Index:
+        buffer_type = D3D11_BIND_INDEX_BUFFER;
+        break;
+    case GpuBufferType_Constant:
+        buffer_type = D3D11_BIND_CONSTANT_BUFFER;
+        cpu_access_flags = D3D11_CPU_ACCESS_WRITE;
+        break;
+    case GpuBufferType_Structure:
+        create_srv = true;
+        format = DXGI_FORMAT_UNKNOWN;
+        cpu_access_flags = D3D11_CPU_ACCESS_WRITE;
+        misc_flags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+        buffer_type = D3D11_BIND_SHADER_RESOURCE;
+        ASSERT(!buf->is_dymamic);
+        struct_byte_stride = (UINT)element_size;
+        break;
+    case GpuBufferType_RWStructure:
+        create_srv = true;
+        create_uav = true;
+        format = DXGI_FORMAT_UNKNOWN;
+        cpu_access_flags = D3D11_CPU_ACCESS_WRITE;
+        misc_flags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+        buffer_type = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS;
+        ASSERT(!buf->is_dymamic);
+        struct_byte_stride = (UINT)element_size;
+        break;
+    case GpuBufferType_AppendStructure:
+        create_srv = true;
+        create_uav = true;
+        format = DXGI_FORMAT_UNKNOWN;
+        cpu_access_flags = D3D11_CPU_ACCESS_WRITE;
+        misc_flags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+        buffer_type = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_UNORDERED_ACCESS;
+        ASSERT(!buf->is_dymamic);
+        struct_byte_stride = (UINT)element_size;
+        uav_flags = D3D11_BUFFER_UAV_FLAG_APPEND;
+        break;
+    case GpuBufferType_IndirectArgs:
+        create_uav = true;
+        buffer_type = D3D11_BIND_UNORDERED_ACCESS;
+        misc_flags = D3D11_RESOURCE_MISC_DRAWINDIRECT_ARGS | D3D11_RESOURCE_MISC_BUFFER_ALLOW_RAW_VIEWS;
+        uav_flags |= D3D11_BUFFER_UAV_FLAG_RAW;
+        format = DXGI_FORMAT_R32_TYPELESS;
+        break;
+    default:
+        FAIL;
+    }
+
+    if (!buf->buffer)
+    {
+        {
+            D3D11_BUFFER_DESC desc;
+            desc.ByteWidth = total_bytes;
+            desc.Usage = usage_flag;
+            desc.BindFlags = buffer_type;
+            desc.CPUAccessFlags = buf->is_dymamic ? D3D11_CPU_ACCESS_WRITE | cpu_access_flags : cpu_access_flags;
+            desc.MiscFlags = misc_flags;
+            desc.StructureByteStride = struct_byte_stride;
+
+            D3D11_SUBRESOURCE_DATA dx11_data;
+            dx11_data.pSysMem = data;
+            dx11_data.SysMemPitch = memory_pitch;
+            dx11_data.SysMemSlicePitch = 0;
+
+			HR(s_dx11.device->CreateBuffer(
+				&desc,                                        //[in]            const D3D11_BUFFER_DESC * pDesc,
+				(data == nullptr) ? nullptr : &dx11_data,     //[in, optional]  const D3D11_SUBRESOURCE_DATA * pInitialData,
+				&buf->buffer                                  //[out, optional] ID3D11Buffer * *ppBuffer
+			));
+            SetResourceName(buf->buffer, buf->name);
+        }
+        DEBUG_LOG("Created and Uploaded data to gpu buffer: element: %i size: %i", element_size, in_count);
+
+        if (create_srv)
+        {
+            D3D11_SHADER_RESOURCE_VIEW_DESC desc;
+            ZeroMemory(&desc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
+            desc.Format = format;
+            desc.ViewDimension = D3D_SRV_DIMENSION_BUFFER;
+            desc.Buffer.FirstElement = 0;
+            desc.Buffer.NumElements = (UINT)in_count;
+            HR(s_dx11.device->CreateShaderResourceView(
+                buf->buffer,                    //[in]            ID3D11Resource * pResource,
+                &desc,                          //[in, optional]  const D3D11_SHADER_RESOURCE_VIEW_DESC * pDesc,
+                &buf->structure_resource_view   //[out, optional] ID3D11ShaderResourceView * *ppSRView
+            ));
+        }
+
+        if (create_uav)
+        {
+            D3D11_UNORDERED_ACCESS_VIEW_DESC desc;
+            ZeroMemory(&desc, sizeof(D3D11_UNORDERED_ACCESS_VIEW_DESC));
+            desc.Format = format;
+            desc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+            desc.Buffer.FirstElement = 0;
+            desc.Buffer.NumElements = (UINT)in_count;
+            desc.Buffer.Flags = uav_flags;
+            HR(s_dx11.device->CreateUnorderedAccessView(
+                buf->buffer,
+                &desc,
+                &buf->unordered_access_view
+            ));
+        }
+
+        return;
+    }
+
+    if (buf->is_dymamic)
+    {
+        //map/unmap/memcopy
+        D3D11_MAPPED_SUBRESOURCE resource;
+        ZeroMemory(&resource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+        HR(s_dx11.device_context->Map(
+            buf->buffer,          //[in]            ID3D11Resource * pResource,
+            0,                      //[in]            UINT                     Subresource,
+            D3D11_MAP_WRITE_DISCARD,//[in]            D3D11_MAP                MapType,
+            0,                      //[in]            UINT                     MapFlags,
+            &resource               //[out, optional] D3D11_MAPPED_SUBRESOURCE * pMappedResource
+        ));
+        memmove(resource.pData, data, element_size * in_count);
+        s_dx11.device_context->Unmap(buf->buffer, 0);
+        DEBUG_LOG("Uploaded dynamic_buffer data to gpu buffer: element: %i size: %i", element_size, in_count);
+    }
+    else
+    {
+        s_dx11.device_context->UpdateSubresource(
+            buf->buffer,  //[in]           ID3D11Resource * pDstResource,
+            0,              //[in]           UINT            DstSubresource,
+            NULL,           //[in, optional] const D3D11_BOX * pDstBox,
+            data,           //[in]           const void* pSrcData,
+            total_bytes,    //[in]           UINT            SrcRowPitch,
+            0               //[in]           UINT            SrcDepthPitch
+        );
+        DEBUG_LOG("Uploaded default_buffer data to gpu buffer: element: %i size: %i", element_size, in_count);
+    }
+}
+
+void GpuBuffer::Bind(u32 slot, GpuBufferBindLocation binding)
+{
+    DX11GpuBuffer* buf = reinterpret_cast<DX11GpuBuffer*>(this);
+    switch (type)
+    {
+    case GpuBufferType_Constant:
+    {
+        switch (binding)
+        {
+        case GpuBufferBindLocation_Vertex:
+            s_dx11.device_context->VSSetConstantBuffers(slot, 1, &buf->buffer);
+            break;
+        case GpuBufferBindLocation_Pixel:
+            s_dx11.device_context->PSSetConstantBuffers(slot, 1, &buf->buffer);
+            break;
+        case GpuBufferBindLocation_All:
+            s_dx11.device_context->VSSetConstantBuffers(slot, 1, &buf->buffer);
+            s_dx11.device_context->PSSetConstantBuffers(slot, 1, &buf->buffer);
+            break;
+        default:
+            FAIL;
+            break;
+        }
+        break;
+    }
+    case GpuBufferType_Structure:
+    {
+        switch (binding)
+        {
+        case GpuBufferBindLocation_Vertex:
+            s_dx11.device_context->VSSetShaderResources(slot, 1, &buf->structure_resource_view);
+            break;
+        case GpuBufferBindLocation_Pixel:
+            s_dx11.device_context->PSSetShaderResources(slot, 1, &buf->structure_resource_view);
+            break;
+        case GpuBufferBindLocation_All:
+            s_dx11.device_context->VSSetShaderResources(slot, 1, &buf->structure_resource_view);
+            s_dx11.device_context->PSSetShaderResources(slot, 1, &buf->structure_resource_view);
+            break;
+        case GpuBufferBindLocation_Compute:
+			s_dx11.device_context->CSSetShaderResources(slot, 1, &buf->structure_resource_view);
+			s_dx11.device_context->CSSetUnorderedAccessViews(slot, 1, &buf->unordered_access_view, nullptr);
+            break;
+        default:
+            FAIL;
+            break;
+        }
+        break;
+    }
+    default:
+        FAIL;
+    }
+}
+
+bool CreateGpuBuffer(GpuBuffer** buffer, const std::wstring& name, bool is_dynamic, GpuBufferType type)
+{
+    ASSERT(buffer);
+    ASSERT(*buffer == nullptr);
+    GfxGpuBuffer* buf = new GfxGpuBuffer;
+
+    buf->is_dymamic = is_dynamic;
+    buf->type = type;
+    buf->name = name;
+    (*buffer) = reinterpret_cast<GpuBuffer*>(buf);
+    return true;
+}
+
+void DeleteBuffer(GpuBuffer** buffer)
+{
+    VALIDATE(buffer);
+    DX11GpuBuffer* buf = reinterpret_cast<DX11GpuBuffer*>(*buffer);
+    SafeRelease(buf->buffer);
+    delete buf;
+    DEBUG_LOG("GPU Buffer deleted %i, %i\n", m_target, m_handle);
+}
